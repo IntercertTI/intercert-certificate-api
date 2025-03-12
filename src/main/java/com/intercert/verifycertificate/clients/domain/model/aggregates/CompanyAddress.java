c24063bcd0404fa003813fcb18746b91dee316d5
neo <defneorise@gmail.com>
2025-03-12 10:16:58 -0500

    feat(clients): add company address aggregate

---
+++ src/main/java/com/intercert/verifycertificate/clients/domain/model/aggregates/CompanyAddress.java
@@ -0,0 +1,33 @@
+package com.intercert.verifycertificate.clients.domain.model.aggregates;
+
+import com.intercert.verifycertificate.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+public class CompanyAddress extends AuditableAbstractAggregateRoot<CompanyAddress> {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "company_id")
+    @NotNull
+    private Company company;
+
+    private String phone;
+
+    @Email
+    private String email;
+
+    @NotNull
+    private String taxDomicle;
+
+    public CompanyAddress(Company company,String phone, String email, String taxDomicle) {
+        this.company = company;
+        this.phone = phone;
+        this.email = email;
+        this.taxDomicle = taxDomicle;
+    }
+
+}
